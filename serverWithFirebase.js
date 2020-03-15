b739ceae8a238e6eb28642e448d35394a2327eeb
paedayz <pae16phasit@gmail.com>
2020-03-15 12:05:44 +0700

    pdayz

--- serverWithFirebase.js
+++ serverWithFirebase.js
@@ -11,104 +11,30 @@ var firebaseConfig = {
     messagingSenderId: "601896548140",
     appId: "1:601896548140:web:b32bca22ebc24925199e89",
     measurementId: "G-36Z1FV8TNV"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  let firestore = firebase.firestore();
+let firestore = firebase.firestore();
 
-  /*
-  firestore.collection("products").doc("Node.js for Beginners").set({
-    id: '1001',
-    name: 'Node.js for Beginners',
-    category: 'Node',
-    price: 990
-    })
-    .then(function() {
-        console.log("Document successfully written!");
-    })
-    .catch(function(error) {
-        console.error("Error writing document: ", error);
+app.get('/products',(req, res) => {
+  firestore.collection("products").get()
+  .then(snapshot => {
+    snapshot.forEach(doc => {
+      console.log(doc.id, '=>' , doc.data());
     });
-
-    
-  firestore.collection("products").add({
-    id: '1001',
-    name: 'Node.js for Beginners',
-    category: 'Node',
-    price: 990
-  }).then ((doc) => {
-    console.info(doc.id);
-  }).catch((error) => {
-    console.error(error);
-  });
-
-  firestore.collection("products").doc("React 101").set({
-    id: '1002',
-    name: 'React 101',
-    category: 'React',
-    price: 3990
-  }).then ((doc) => {
-    console.info(doc.id);
-  }).catch((error) => {
-    console.error(error);
-  });
-
-  firestore.collection("products").doc("Getting started with MongoDB").set({
-    id: '1003',
-    name: 'Getting started with MongoDB',
-    category: 'MongoDB',
-    price: 1990
-  }).then ((doc) => {
-    console.info(doc.id);
-  }).catch((error) => {
-    console.error(error);
-  });*/
-
-  app.post('/products', (req, res) => {
-  const payload = req.body;
-  firestore.collection("products").add(payload)
-  .then ((doc) => {
-    console.info(doc.id);
-  }).catch((error) => {
-    console.error(error);
-});
-
-app.get('/products', async (req, res) => {
-    firestore.collection("store").doc("Hatsune Miku").get().then((docs) => {
-        console.log(docs.data());
-        res.send(docs.data());
-      });
-});
-/*
-app.get('/products/:id', async (req, res) => {
-  const { id } = req.params
-  firestore.collection("products").where("id","==",id).get().then
-  (function(snapshot){
-    snapshot.forEach(function(docs){
-      console.log(docs.data());
-      res.send(docs.data());
-    })
+  })
+  .catch(err => {
+    console.log('Error getting documents', err);
   })
 });
 
-app.put('/products/:id', async (req, res) => {
+app.post('/products',async(req, res) => {
   const payload = req.body;
-  const { id } = req.params
-
-  const product = await imProduct.findByIdAndUpdate(id, { $set: payload });
- 
-  res.json(product)
-})
-
-app.delete('/products/:id', async (req, res) => {
-  const { id } = req.params;
-
-  await imProduct.findByIdAndDelete(id);
-  res.status(204).end();
+  firestore.collection("products").add(payload);
 });
- 
+
 const port = process.env.port || 3030;
-app.listen(port, () => {
-  console.log("Connecting to port", port);
-})*/
+app.listen(port, function() {
+  console.log("Connecting to port", port)
+})
--- testServerWithFirebase.js
+++ testServerWithFirebase.js
@@ -11,39 +11,39 @@ var firebaseConfig = {
     messagingSenderId: "601896548140",
     appId: "1:601896548140:web:b32bca22ebc24925199e89",
     measurementId: "G-36Z1FV8TNV"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  let firestore = firebase.firestore();
+let firestore = firebase.firestore();
 
-  /*ADD*/
-  // firestore.collection("store").doc("Hatsune Miku").set({
-  //   item: "Nendorroid",
-  //   id: 33,
-  //   available: true
-  // }).then ((doc) => {
-  //   console.info(doc.id);
-  // }).catch((error) => {
-  //   console.error(error);
-  // });
+/*ADD*/
+// firestore.collection("store").doc("Hatsune Miku").set({
+//   item: "Nendorroid",
+//   id: 33,
+//   available: true
+// }).then ((doc) => {
+//   console.info(doc.id);
+// }).catch((error) => {
+//   console.error(error);
+// });
 
-  /*DELETE*/
-  //firestore.collection("store").doc("QAAiTr9lxKMwvGC4cn2o").delete();
+/*DELETE*/
+//firestore.collection("store").doc("QAAiTr9lxKMwvGC4cn2o").delete();
 
-  /*UPDATE*/
-  // firestore.collection("store").doc("Hatsune Miku").update({
-  //   sell_date: "30.1.20";
-  // });
+/*UPDATE*/
+// firestore.collection("store").doc("Hatsune Miku").update({
+//   sell_date: "30.1.20";
+// });
 
-  app.get('/',(req, res) => {
-    firestore.collection("store").doc("Hatsune Miku").get().then((docs) => {
-      console.log(docs.data());
-      res.send(docs.data());
-    })
-  });
- 
-  const port = process.env.port || 3030;
-  app.listen(port, () => {
-    console.log("Connecting to port", port);
+app.get('/',(req, res) => {
+  firestore.collection("store").doc("Hatsune Miku").get().then((docs) => {
+    console.log(docs.data());
+    res.send(docs.data());
   })
+});
+ 
+const port = process.env.port || 3030;
+app.listen(port, () => {
+  console.log("Connecting to port", port);
+})
