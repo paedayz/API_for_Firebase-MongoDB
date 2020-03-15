4f4d12753e6f3fce622b4f479abaea50f63e580c
paedayz <pae16phasit@gmail.com>
2020-03-15 18:09:00 +0700

    pdayz

--- testServerWithFirebase.js
+++ testServerWithFirebase.js
@@ -18,32 +18,63 @@ firebase.initializeApp(firebaseConfig);
 let firestore = firebase.firestore();
 
 /*ADD*/
-// firestore.collection("store").doc("Hatsune Miku").set({
-//   item: "Nendorroid",
-//   id: 33,
+// firestore.collection("store").doc("test").set({
+//   item: "wow",
+//   id: 23,
 //   available: true
-// }).then ((doc) => {
-//   console.info(doc.id);
-// }).catch((error) => {
-//   console.error(error);
-// });
+// })
+
 
 /*DELETE*/
 //firestore.collection("store").doc("QAAiTr9lxKMwvGC4cn2o").delete();
+
 
 /*UPDATE*/
 // firestore.collection("store").doc("Hatsune Miku").update({
 //   sell_date: "30.1.20";
 // });
 
-app.get('/',(req, res) => {
-  firestore.collection("store").doc("Hatsune Miku").get().then((docs) => {
-    console.log(docs.data());
-    res.send(docs.data());
-  })
-});
+
+/*Another Add*/
+// firestore.collection("store").doc("Hatsune Miku").set({
+//   item: "Nendoroid",
+//   id: 33,
+//   available: true
+// });
+// firestore.collection("store").doc("Flandre Scarlet").set({
+//   item: "Nendoroid",
+//   id: 136,
+//   available: true
+// });
+// firestore.collection("store").doc("Yae Sakura").set({
+//   item: "Nendoroid",
+//   id: 908,
+//   available: false
+// });
+
+
+/*Batch*/
+// let batch = firestore.batch(),
+//     miku = firestore.collection("store").doc("Hatsune Miku"),
+//     flandre = firestore.collection("store").doc("Flandre Scarlet");
+// batch.update(miku, {
+//     available: true
+// });
+// batch.update(flandre, {
+//     available: false
+// });
+// batch.commit();
+
+
+/*Connect Server */
+// app.get('/',(req, res) => {
+//   firestore.collection("store").doc("Hatsune Miku").get().then((docs) => {
+//     console.log(docs.data());
+//     res.send(docs.data());
+//   })
+// });
  
-const port = process.env.port || 3030;
-app.listen(port, () => {
-  console.log("Connecting to port", port);
-})
+// const port = process.env.port || 3030;
+// app.listen(port, () => {
+//   console.log("Connecting to port", port);
+// })
